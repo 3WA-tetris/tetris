2ac579362d393200db8911b7a2373793ed6740e4
xdebbie <deh@live.in>
2020-06-05 02:14:05 +0200

    update on the main game.js file

--- src/game.js
+++ src/game.js
@@ -1,9 +1,10 @@
 export default class Game {
-  // initialisation of the game with score, lines and level
+  // initialisation of the game with the score, lines and level
   score = 0;
   lines = 0;
   level = 0;
-  // grid to start the game
+
+  // canvas to start the game
   playfield = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -26,6 +27,7 @@ export default class Game {
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
   ];
+
   // active piece with position x and y
   activePiece = {
     x: 0,
@@ -38,18 +40,85 @@ export default class Game {
     ]
   };
 
-  // move left of active piece
+  // move active piece to the left
   movePieceLeft() {
     this.activePiece.x -= 1;
+
+    if (this.hasCollision()) {
+      this.activePiece.x += 1;
+    }
   }
 
-  // move right of active piece
+  // move active piece to the right
   movePieceRight() {
     this.activePiece.x += 1;
+
+    if (this.hasCollision()) {
+      this.activePiece.x -= 1;
+    }
   }
 
-  // move down of active piece
+  // move active piece down
   movePieceDown() {
-    this.activePiece.x += 1;
+    this.activePiece.y += 1;
+
+    if (this.hasCollision()) {
+      this.activePiece.y -= 1;
+      this.lockPiece();
+    }
+  }
+
+  /* check if the piece touches the wall and then block its position,
+  so it doesn't disappear further in the canvas */
+  hasCollision() {
+    const {
+      y: pieceY,
+      x: pieceX,
+      blocks
+    } = this.activePiece;
+
+    /* the for loop will roam through the pieces' y and x's axises 
+    (as per the constant above, so for example, a T piece in the horizontal
+    position [_|_] will occupy a length of 3 blocks on the x axis and 2 blocks
+    on the y axis), so it can define the piece's shape and check whether any of
+    its extremities is touching a wall */
+    for (let y = 0; y < blocks.length; y++) {
+      for (let x = 0; x < blocks[y].length; x++) {
+        /* the if statement checks whether the whole shape of the piece exists ('blocks[x][y]',
+          in this case a 3x3 block size) AND whether the actual absolute position of the 
+          piece on the canvas (represented by 'pieceY') PLUS one y increment is equal to 
+          undefined (meaning, the piece can fit in its next advance to the adjacent y cell) 
+          OR whether the actual absolute position of the piece on the canvas can reach a corner or 
+          a vertical wall ('pieceY + y' and 'pieceX + x') if there is a next x and y increments.
+          If the condition holds true, then it will return true and proceed until the piece gets to 
+          the end of the loop and return a FALSE signal, triggering the lockPiece() function once 
+          the piece reaches the bottom of the canvas. 
+          Remember that on boolean 1 = true and 0 = false */
+        if (blocks[y][x] && ((this.playfield[pieceY + y] === undefined || this.playfield[pieceY + y][pieceX + x] === undefined)) || this.playfield[pieceY + y][pieceX + x]) {
+          return true;
+        }
+      }
+    }
+
+    return false;
+  }
+
+  // lock the active piece in its actual position once it reaches the bottom
+  lockPiece() {
+    const {
+      y: pieceY,
+      x: pieceX,
+      blocks
+    } = this.activePiece;
+
+    for (let y = 0; y < blocks.length; y++) {
+      for (let x = 0; x < blocks[y].length; x++) {
+        if (blocks[y][x]) {
+          /* simply writes on the canvas the shape of the piece when this function
+          is called by the movePieceDown */
+          this.playfield[pieceY + y][pieceX + x] = blocks[y][x];
+        }
+      }
+    }
   }
 }
