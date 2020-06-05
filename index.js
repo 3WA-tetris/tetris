4cae47229c28049c64a89119e8cc6ef4de606c25
xdebbie <deh@live.in>
2020-06-05 19:36:35 +0200

    Canvas drawing update

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+/* ---------------------------------------
+$   INDEX
+-----------------------------------------*/
+
 import Game from './src/game.js';
+import View from './src/view.js';
+
+const root = document.querySelector('#root');
 
 const game = new Game();
+const view = new View(root, 480, 640, 20, 10);
 
 window.game = game;
+window.view = view;
 
-console.log(game);
+view.renderPlayfield(game.playfield);
